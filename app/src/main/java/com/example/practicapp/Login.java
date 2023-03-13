d401246f16f577abbfebfae92642933e3e694b7e
kazandr225 <danii.kazanskiy@mail.ru>
2023-03-13 13:07:19 +0300

    asd

--- app/src/main/java/com/example/practicapp/Login.java
+++ app/src/main/java/com/example/practicapp/Login.java
@@ -5,8 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
 
 public class Login extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/practicapp/RetrofitAPI.java
@@ -0,0 +1,8 @@
+package com.example.practicapp;
+
+
+
+public interface RetrofitAPI {
+
+
+}
